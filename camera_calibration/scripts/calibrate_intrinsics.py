c5de31770ac9917ae83a0336f7776ebd56b63c0d
Michael <michael@Michaels-MacBook-Pro.local>
2020-01-18 18:14:45 -0500

    created script to intrinsicly calibrate a set of cameras

--- camera_calibration/scripts/calibrate_intrinsics.py
+++ camera_calibration/scripts/calibrate_intrinsics.py
@@ -1,31 +1,39 @@
-cameraNames = ['poc_1','poc_2','poc_3','poc_4']
-calibrationDir = '../calibration_data'
-imageExtension = '.png'
-displayResolution = (1280,720)
-
 import os
 import cv2
 import sys
 import glob
-
+import argparse
 sys.path.insert(1, '../code')
 from calibration import IntrinsicCalibrator
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-d', '--dir', required = True, help = 'Path to the camera folders')
+ap.add_argument('-c', '--cameras', nargs='+', required = True, help = 'List of folder names')
+ap.add_argument('-e', '--extension', help = 'Image extension', default='.png')
+ap.add_argument('--height', help = 'Image height', default=720)
+ap.add_argument('--width', help = 'Image width', default=1280)
+args = vars(ap.parse_args())
+cameraNames = args['cameras']
+calibrationDir = args['dir']
+imageExtension = args['extension']
+displayResolution = (args['width'], args['height'])
+
 basePath = os.path.abspath('')
-calibrationPath = os.path.join(basePath,calibrationDir)
+calibrationPath = os.path.join(basePath, calibrationDir)
 calibrator = IntrinsicCalibrator()
 
 for cameraName in cameraNames:
-    paramPath = os.path.join(calibrationPath,cameraName,cameraName+'.yaml')
-    imgPaths = glob.glob(os.path.join(calibrationPath,cameraName,'*'+imageExtension))
+    paramPath = os.path.join(calibrationPath, cameraName, cameraName + '.yaml')
+    imgPaths = glob.glob(os.path.join(calibrationPath, cameraName, '*' + imageExtension))
     calibrator.calibrate_from_images(paramPath, imgPaths)
     calibrator.load_params(paramPath)
 
-    img = cv2.imread(os.path.join(calibrationPath,cameraName,'test'+imageExtension))
+    img = cv2.imread(os.path.join(calibrationPath, cameraName, 'test' + imageExtension))
     assert img is not None, "Need a test image named test.{extension}"
     out = calibrator.undistort_image(img)
-    cv2.imshow('frame',out)
-    while(True):
+    cv2.putText(out, f'Test results for {cameraName} undistorsion. Press "space" to continue.',(20, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (179, 72, 19), 2, cv2.LINE_AA, False)
+    cv2.imshow('frame', out)
+    while (True):
         key = cv2.waitKey(1)
-        if key == 32: #spacebar
+        if key == 32:  # spacebar
             break
